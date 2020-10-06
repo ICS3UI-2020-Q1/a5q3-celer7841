438724ee2c1a38cf713185376177030ad941d505
celer7841 <celer7841@wrdsb.ca>
2020-10-06 16:27:06 +0000

    finished

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @author celer7841
  */
 public class Main {
 
@@ -9,7 +10,19 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create a variable to keep track of what number we are on 
+    int count = 1;
+    // the accumulator to mutliply the count into
+    int total = 1;
+    while(count <= 11){
+      // mutliply the numbers as we go into total
+      total = total * count;
+      // mutliply 1 to our count to increase it 
+      count = count + 1;
+    }
+      // print the total to the screen 
+    System.out.println("Please enter an integer to calculate the factorial of"); 
+    System.out.println("The total is " + total);
+
   }
 }
